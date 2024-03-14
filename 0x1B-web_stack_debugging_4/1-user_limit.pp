de08a1f7524ef5cbc8ce0609039ac6a48cdc7650
root <habibaragab324@gmail.com>
2024-03-14 19:39:31 +0000

    lol

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-# Enable the user holberton to login and open files without error.
+# To enable the user holberton to login and open files without error.
 
 # Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
@@ -10,3 +10,4 @@ exec { 'increase-hard-file-limit-for-holberton-user':
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
+}
